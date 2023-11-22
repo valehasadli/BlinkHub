5c0f0e041cf6370b77a8b4c654f8231f604f05c3
Valeh ASADLI <valehasadli@gmail.com>
2023-11-22 13:23:00 +0100

    Implement event listener priority queue for optimized event handling (#45)
    * feat: performance metrics
    
    * removed temp benchmark
    
    * Implement event listener priority queue for optimized event handling (#44)
    
    * refactor: Implement event listener priority queue for optimized event handling
    
    * refactor: Implement event listener priority queue for optimized event handling
    
    * refactor: improved test for complex scenario
    
    * removed temp benchmark
    
    * version and deps
    
    * refactor: Implement event listener priority queue for optimized event handling

---
+++ src/core/PriorityQueue.ts
@@ -0,0 +1,26 @@
+import { Listener } from "../types";
+
+export default class PriorityQueue<T extends (...args: any[]) => any> {
+	private items: Array<Listener<T>> = [];
+
+	enqueue(listener: Listener<T>): void {
+		let index = this.items.findIndex(item => item.priority < listener.priority);
+		index = index === -1 ? this.items.length : index;
+		this.items.splice(index, 0, listener);
+	}
+
+	getListeners(): Array<Listener<T>> {
+		return [...this.items];
+	}
+
+	remove(listenerToRemove: Listener<T>): void {
+		const index = this.items.findIndex(listener =>
+			listener.callback === listenerToRemove.callback &&
+			listener.priority === listenerToRemove.priority
+		);
+
+		if (index !== -1) {
+			this.items.splice(index, 1);
+		}
+	}
+}

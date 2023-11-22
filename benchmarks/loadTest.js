ac6a1672440d712b5314d700402fe0e8a56780d0
Valeh ASADLI <valehasadli@gmail.com>
2023-11-22 13:04:58 +0100

    refactor: improved test for complex scenario

--- benchmarks/loadTest.js
+++ benchmarks/loadTest.js
@@ -2,16 +2,43 @@ const Emitter = require('../dist/core/Emitter').default;
 
 const userEmitter = new Emitter();
 
+// High-priority listener for userLoggedIn
 userEmitter.subscribe('userLoggedIn', (username) => {
-    console.log(`${username} logged in`);
+    console.log(`[High Priority] ${username} logged in`);
+}, 100);
+
+// Medium-priority listener for userLoggedIn
+userEmitter.subscribe('userLoggedIn', (username) => {
+    for (let i = 0; i < 1000; i++) {} // Simulate processing delay
+    console.log(`[Medium Priority] Welcome message sent to ${username}`);
+}, 50);
+
+// Low-priority listener for userLoggedIn
+userEmitter.subscribe('userLoggedIn', (username) => {
+    for (let i = 0; i < 10000; i++) {} // Simulate heavier task
+    console.log(`[Low Priority] Analytics updated for ${username}`);
+}, 10);
+
+// Additional listeners for systemCheck and dataUpdate
+userEmitter.subscribe('systemCheck', (message) => {
+    console.log(`[System Check] ${message}`);
+});
+
+userEmitter.subscribe('dataUpdate', (message) => {
+    console.log(`[Data Update] ${message}`);
 });
 
 console.time('emitLoop');
 
-for (let i= 0; i < 1000000; i++) {
+// Emitting a mix of events
+for (let i = 0; i < 1000000; i++) {
     userEmitter.emit('userLoggedIn', `User${i}`);
+    if (i % 100 === 0) {
+        userEmitter.emit('systemCheck', `System Check at ${i}`);
+        userEmitter.emit('dataUpdate', `Data Update at ${i}`);
+    }
 }
 
 console.timeEnd('emitLoop');
 
-module.exports = Emitter; // Or the appropriate export statement
+module.exports = Emitter;
